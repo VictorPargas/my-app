22188d6cb40bf1e5c34fcfab4180e6c599a519c4
Victor Parga <victorparga765@gmail.com>
2024-02-01 16:40:45 -0300

    feat: and initial Search Component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,26 @@
-import Image from "next/image";
+import { format } from "date-fns";
 import Header from "../_components/header";
+import { ptBR } from "date-fns/locale";
+import Search from "./_components/search";
 
 export default function Home() {
   return ( 
     <div>
       <Header />
+    <div className="px-5 pt-5">
+      <h2 className="text-xl font-bold">Olá, Parga</h2>
+      <p className="capitalize text-sm">
+        {format(new Date(), "EEEE',' dd 'de' MMMM", {
+        locale: ptBR,
+      })}
+      </p>
     </div>
-  )
+
+      <div className="px-5 mt-6">
+        <Search />
+      </div>
+   
+  </div>
+  );
 }
   
