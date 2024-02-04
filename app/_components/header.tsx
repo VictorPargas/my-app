e59d65d622835408bc0c05f124b23c30587c07f9
Victor Parga <victorparga765@gmail.com>
2024-02-04 01:37:20 -0300

    feat: add auth Provider

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import Image from "next/image";
 import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
@@ -8,9 +10,9 @@ const Header = () => {
         <Card>
             <CardContent className="p-5 justify-between items-center flex flex-row">       
             <Image src="/logo3.png" alt="luminousBarber" height={18} width={150} />
-            <Button variant="outline" size="icon" className="">
-                <MenuIcon size={18} />
-            </Button>
+            <Button variant="outline" size="icon" className="h-8 w-8">
+                <MenuIcon size={16} />
+                </Button>
             </CardContent>
         </Card>
     );
