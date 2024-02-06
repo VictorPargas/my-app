bc25ec338378103aaf035b7f43fac98c4753cfd3
Victor Parga <victorparga765@gmail.com>
2024-02-06 00:24:44 -0300

    feat: seed alter

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -29,50 +29,34 @@ async function seedDatabase() {
     ];
     // Nomes criativos para as barbearias
     const creativeNames = [
-      "Barbearia Vintage",
-      "Corte & Estilo",
-      "Barba & Navalha",
-      "The Dapper Den",
-      "Cabelo & Cia.",
-      "Machado & Tesoura",
-      "Barbearia Elegance",
-      "Aparência Impecável",
-      "Estilo Urbano",
-      "Estilo Clássico",
+      "Luminous Barbearia",
+      
     ];
 
     // Endereços fictícios para as barbearias
     const addresses = [
-      "Rua da Barbearia, 123",
-      "Avenida dos Cortes, 456",
-      "Praça da Barba, 789",
-      "Travessa da Navalha, 101",
-      "Alameda dos Estilos, 202",
-      "Estrada do Machado, 303",
-      "Avenida Elegante, 404",
-      "Praça da Aparência, 505",
-      "Rua Urbana, 606",
-      "Avenida Clássica, 707",
+      "Rua Gerônimo Gregório silva, 179, Jardim Sonia Maria, Mauá SP",
+      
     ];
 
     const services = [
       {
-        name: "Corte de Cabelo",
+        name: "Corte e Barba",
         description: "Estilo personalizado com as últimas tendências.",
-        price: 60.0,
+        price: 50.0,
+        imageUrl: "https://utfs.io/f/0ddfbd26-a424-43a0-aaf3-c3f1dc6be6d1-1kgxo7.png",
+      },
+      {
+        name: "Corte",
+        description: "Modelagem completa para destacar sua masculinidade.",
+        price: 40.0,
         imageUrl: "https://utfs.io/f/0ddfbd26-a424-43a0-aaf3-c3f1dc6be6d1-1kgxo7.png",
       },
       {
         name: "Barba",
-        description: "Modelagem completa para destacar sua masculinidade.",
-        price: 40.0,
-        imageUrl: "https://utfs.io/f/e6bdffb6-24a9-455b-aba3-903c2c2b5bde-1jo6tu.png",
-      },
-      {
-        name: "Pézinho",
         description: "Acabamento perfeito para um visual renovado.",
-        price: 35.0,
-        imageUrl: "https://utfs.io/f/8a457cda-f768-411d-a737-cdb23ca6b9b5-b3pegf.png",
+        price: 30.0,
+        imageUrl: "https://utfs.io/f/e6bdffb6-24a9-455b-aba3-903c2c2b5bde-1jo6tu.png",
       },
       {
         name: "Sobrancelha",
@@ -81,10 +65,10 @@ async function seedDatabase() {
         imageUrl: "https://utfs.io/f/2118f76e-89e4-43e6-87c9-8f157500c333-b0ps0b.png",
       },
       {
-        name: "Massagem",
+        name: "Pézinho",
         description: "Relaxe com uma massagem revigorante.",
-        price: 50.0,
-        imageUrl: "https://utfs.io/f/c4919193-a675-4c47-9f21-ebd86d1c8e6a-4oen2a.png",
+        price: 15.0,
+        imageUrl: "https://utfs.io/f/8a457cda-f768-411d-a737-cdb23ca6b9b5-b3pegf.png",
       },
       {
         name: "Hidratação",
